05d3408f70cc1aa010f4bebe05da0f91a614fbb3
daluamon <marcio.o.alves@hotmail.com>
2024-02-16 21:18:14 -0300

    Drag and Drop

---
+++ actions/update-card-order/index.ts
@@ -0,0 +1,51 @@
+"use server";
+
+import { auth } from '@clerk/nextjs';
+import { InputType, ReturnType } from './types';
+import { db } from '@/lib/db';
+import { revalidatePath } from 'next/cache';
+import { UpdateCardOrder } from './schema';
+import { createSafeAction } from '@/lib/create-safe-action';
+
+
+const handler = async (data: InputType): Promise<ReturnType> => {
+  const {userId, orgId} = auth();
+
+  if (!userId || !orgId) {
+    return {
+      error: "unauthorized",
+    }
+  }
+
+  const { items, boardId } = data;
+
+  let updatedCards;
+   try {
+    const transaction = items.map((card) => 
+      db.card.update({
+        where: {
+          id: card.id,
+          list: {
+            board: {
+              orgId
+            }
+          }
+        },
+        data: {
+          order: card.order,
+          listId: card.listId
+        }
+      })
+    );
+    updatedCards = await db.$transaction(transaction);
+   } catch (error) {
+    return {
+      error: "Falha ao atualizar a ordem dos cartões. Por favor, tente novamente."
+    }
+   }
+
+   revalidatePath(`/board/${boardId}`);
+   return { data: updatedCards };
+}
+
+export const updateCardOrder = createSafeAction(UpdateCardOrder, handler);
